bfaac22e423d9c563f0126f450ff9716f3b6529d
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-28 20:47:27 +0200

    Add Book Model

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
+
