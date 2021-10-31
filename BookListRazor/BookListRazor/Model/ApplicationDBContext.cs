466219ad7bf7d4c28f6f7934cbd5c8b3c08f23d1
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-31 22:25:08 +0100

    Install Entity Frameworkand create database. DB connection not working properly.

---
+++ BookListRazor/BookListRazor/Model/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Book> Book { get; set; }
+    }
+}
